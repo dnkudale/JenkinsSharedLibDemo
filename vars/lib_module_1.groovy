12b4dde34bcf07035aade23d275d5c521ec36e09
dnkudale <dnkudale97@gmail.com>
2025-06-12 15:01:30 +0530

    Update lib_module_1.groovy

--- vars/lib_module_1.groovy
+++ vars/lib_module_1.groovy
@@ -1,4 +1,3 @@
-def func_from_module_1(){
-    println("This is a test function from module 1 and function func_from_module_1")
-    println("This function can be used in any pipeline stages")
+def func_from_module_1(name){
+  echo "Hey ${name}, Welcome to My Program"
 }
